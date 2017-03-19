4ff00bf60faa8995b012e94c2b88c82879966c73
tom <gittom@skink.co.uk>
2017-03-19 07:28:19 +0000

    Trying to track how close we are to solution.

--- Countdown.py
+++ Countdown.py
@@ -1,20 +1,28 @@
 import sys
 
-def recurse(thirdList, myTarget, myoperator = None):
+
+def recurse(thirdList: list, myTarget: int, myoperator: str = None, difference: int = None) -> object:
     if (myoperator == None):
         for operator in operators:
             recurse(thirdList, myTarget, operator)
         return
+    if (difference == None):
+        difference = myTarget
 
-    newValue = "(" + str(thirdList[0])+ myoperator + str(thirdList[1]) + ")"
+    newValue = "(" + str(thirdList[0]) + myoperator + str(thirdList[1]) + ")"
     fourthList = thirdList.copy()
     del fourthList[0]
     fourthList[0] = newValue
-    if (len(fourthList) >1):
-        recurse(fourthList,myTarget)
+    if len(fourthList) > 1:
+        recurse(fourthList, myTarget, None, difference)
     else:
-        answer =eval(fourthList[0])
+        answer = eval(fourthList[0])
+        newdifference = abs(answer - myTarget)
+        if newdifference < difference:
+            difference = newdifference
+            print("** " + str(answer))
         if (answer == myTarget):
+            print("")
             print(fourthList, end="")
             print("           = " + str(answer))
             exit(0)
@@ -29,9 +37,9 @@ def calc(myList, myTarget):
     n = 0
     listlen = len(myList)
     while i < listlen:
+        print("")
         j = 0
         while j < listlen:
-            print(".")
             if (i != j):
                 k = 0
                 while k < listlen:
@@ -46,11 +54,10 @@ def calc(myList, myTarget):
                                         n = 0
                                         while n < listlen:
                                             if (i != n and j != n and k != n and l != n and m != n):
-                                                #print(str(i) + str(j) + str(k) + str(l) + str(m) + str(n))
+                                                # print(str(i) + str(j) + str(k) + str(l) + str(m) + str(n))
                                                 newList = [myList[i], myList[j], myList[k], myList[l], myList[m],
                                                            myList[n]]
                                                 recurse(newList, myTarget)
-
                                             n += 1
                                     m += 1
                             l += 1
@@ -75,7 +82,10 @@ def main():
 
     calc(myList, myTarget)
 
+    print("")
+    print("Solution not found.")
     exit(0)
+
 
 operators = ["+", "-", "*", "/", "*0+"]
 if __name__ == '__main__':
